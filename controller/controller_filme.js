1f3b6f7e7f6a965f86dbce383b7189d924400b92
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2024-02-29 12:31:21 -0300

    em progresso...

--- controller/controller_filme.js
+++ controller/controller_filme.js
@@ -8,8 +8,45 @@ const message = require('../module/config')
 
 const filmesDAO = require('../model/DAO/filme.js')
 
-const setInserirNovoFilme = async () => {
+const setInserirNovoFilme = async (dadosFilme) => {
+    let novoFilmeJSON = {}
 
+    if( dadosFilme.nome == ''            || dadosFilme.nome == undefined            || dadosFilme.nome.length > 80             ||
+        dadosFilme.sinopse == ''         || dadosFilme.sinopse == undefined         || dadosFilme.sinopse.length > 65000       ||
+        dadosFilme.duracao == ''         || dadosFilme.duracao == undefined         || dadosFilme.duracao.length > 8           ||
+        dadosFilme.data_lancamento == '' || dadosFilme.data_lancamento == undefined || dadosFilme.data_lancamento.length != 10 ||
+        dadosFilme.foto_capa == ''       || dadosFilme.foto_capa == undefined       || dadosFilme.fot_capa.length > 200        ||
+        dadosFilme.valor_unitario.length > 6
+    ){
+        return message.ERROR_REQUIRED_FIELDS
+
+    } else {
+
+        let validateStatus = false
+
+        if(dadosFilme.data_relancamento != null || dadosFilme.data_relancamento != ''){
+            if(dadosFilme.data_relancamento.length != 10){
+                return message.ERROR_REQUIRED_FIELDS
+            } else {
+                validateStatus = true
+            }
+        } else {
+            validateStatus = true
+        }
+
+        let novoFilme = await filmesDAO.insertFilme(dadosFilme)
+
+        if(novoFilme){
+            novoFilmeJSON.filme = dadosFilme
+            novoFilmeJSON.status = message.SUCCESS_CREATED_ITEM.status
+            novoFilmeJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code
+            novoFilmeJSON.message = message.SUCCESS_CREATED_ITEM.message
+
+            return novoFilmeJSON
+        } else {
+            return message.ERROR_INTERNAL_SERVER_DB
+        }
+    }
 }
 
 const setAtualizarFilme = async () => {
--- module/config.js
+++ module/config.js
@@ -5,13 +5,19 @@
  * Versão: 1.0
  ******************************************************************************************************************/
 
-/*******************************************MENSAGENS DE ERRO DO PROJETO*******************************************/
-const ERROR_INVALID_ID = {status:false, status_code: 400, message: 'O ID encaminhado na requisição não é válido.'}
-const ERROR_NOT_FOUND = {status: false, status_code: 404, message : 'Não foi encontrado nenhum item.'}
-const ERROR_INTERNAL_SERVER_DB = {status: false, status_code: 500, message: 'Não foi possível processar a requisição, devido a um erro no acesso ao Banco de Dados. Contate o administrador da API!'}
+/*******************************************MENSAGENS DE ERRO*******************************************/
+const ERROR_INVALID_ID          = {status: false, status_code: 400, message: 'O ID encaminhado na requisição não é válido.'}
+const ERROR_REQUIRED_FIELDS     = {status: false, status_code: 400, message: 'Existem campos requeridos que não forma prenchidos o unão atendem aos critérios de digitação.'}
+const ERROR_NOT_FOUND           = {status: false, status_code: 404, message : 'Não foi encontrado nenhum item.'}
+const ERROR_INTERNAL_SERVER_DB  = {status: false, status_code: 500, message: 'Não foi possível processar a requisição, devido a um erro no acesso ao Banco de Dados. Contate o administrador da API!'}
+
+/*******************************************MENSAGENS DE SUCESSO*******************************************/
+const SUCCESS_CREATED_ITEM      = {status: true, status_code: 201, message : 'Item criado com sucesso!'}
 
 module.exports = {
     ERROR_INVALID_ID,
     ERROR_NOT_FOUND,
-    ERROR_INTERNAL_SERVER_DB
+    ERROR_REQUIRED_FIELDS,
+    ERROR_INTERNAL_SERVER_DB,
+    SUCCESS_CREATED_ITEM
 }
