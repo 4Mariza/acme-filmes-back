0a07402d11f8fdc4216bf6223f633a7532bdd823
4Mariza <maria.luizagomedds1@gmail.com>
2024-03-27 23:03:07 -0300

    validação categoria...

--- controller/controller_filme.js
+++ controller/controller_filme.js
@@ -16,9 +16,11 @@ const setInserirNovoFilme = async (dadosFilme, contentType) => {
 
             if (dadosFilme.nome == '' || dadosFilme.nome == undefined || dadosFilme.nome == null || dadosFilme.nome.length > 80 ||
                 dadosFilme.sinopse == '' || dadosFilme.sinopse == undefined || dadosFilme.sinopse == null || dadosFilme.sinopse.length > 65000 ||
+                dadosFilme.categoria == '' || dadosFilme.categoria == undefined || dadosFilme.categoria == null || dadosFilme.categoria.length > 50 ||
                 dadosFilme.duracao == '' || dadosFilme.duracao == undefined || dadosFilme.duracao == null || dadosFilme.duracao.length > 8 ||
                 dadosFilme.data_lancamento == '' || dadosFilme.data_lancamento == undefined || dadosFilme.data_lancamento == null || dadosFilme.data_lancamento.length != 10 ||
                 dadosFilme.foto_capa == '' || dadosFilme.foto_capa == undefined || dadosFilme.foto_capa == null || dadosFilme.foto_capa.length > 400 ||
+                dadosFilme.foto_fundo == '' || dadosFilme.foto_fundo == undefined || dadosFilme.foto_fundo == null || dadosFilme.foto_fundo.length > 400 ||
                 dadosFilme.valor_unitario.length > 6
             ) {
                 return message.ERROR_REQUIRED_FIELDS
@@ -73,9 +75,11 @@ const setAtualizarFilme = async (dadosFilme, id, contentType) => {
             if (id == '' || id == undefined || isNaN(id) ||
                 dadosFilme.nome == '' || dadosFilme.nome == undefined || dadosFilme.nome == null || dadosFilme.nome.length > 80 ||
                 dadosFilme.sinopse == '' || dadosFilme.sinopse == undefined || dadosFilme.sinopse == null || dadosFilme.sinopse.length > 65000 ||
+                dadosFilme.categoria == '' || dadosFilme.categoria == undefined || dadosFilme.categoria == null || dadosFilme.categoria.length > 50 ||
                 dadosFilme.duracao == '' || dadosFilme.duracao == undefined || dadosFilme.duracao == null || dadosFilme.duracao.length > 8 ||
                 dadosFilme.data_lancamento == '' || dadosFilme.data_lancamento == undefined || dadosFilme.data_lancamento == null || dadosFilme.data_lancamento.length != 10 ||
                 dadosFilme.foto_capa == '' || dadosFilme.foto_capa == undefined || dadosFilme.foto_capa == null || dadosFilme.foto_capa.length > 400 ||
+                dadosFilme.foto_fundo == '' || dadosFilme.foto_fundo == undefined || dadosFilme.foto_fundo == null || dadosFilme.foto_fundo.length > 400 ||
                 dadosFilme.valor_unitario.length > 6
             ) {
                 return message.ERROR_REQUIRED_FIELDS
